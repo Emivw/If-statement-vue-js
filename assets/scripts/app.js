937ab95b7cd9e6ea789d7e187f2ef32441f58df8
Emivw <emiya.vanwyk@gmail.com>
2022-07-04 16:10:45 +0200

    first

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,26 @@
 const app = Vue.createApp({
-    template: `
-    <div v-if="isVisible" id="box1">
-    <p>{{greeting}}</p>
+    template: ` <div v-if='isVisible' id='box1'>
+    <p>{{name}} {{age}}</p>
     </div>
-    <input type="text" v-model= "greeting"/>
-    <button v-on:click="display">Click Me</button>
-    `,
+    <input type='text' v-model= 'name'/>
+     <input type='text' v-model= 'age'/>
+    <button v-on:click='display'>Click Me</button>`,
     data: function() {
         return {
-            greeting: "Bonjour",
+            name: "emile",
+            age: 22,
             isVisible: true,
         };
     },
     methods: {
         display() {
-            this.isVisible = !this.isVisible;
-        }
-    }
+            if (parseInt(this.age) > 17) {
+                this.isVisible = true;
+                console.log("user");
+            } else {
+                this.isVisible = false;
+            }
+        },
+    },
 });
 app.mount("#app");
