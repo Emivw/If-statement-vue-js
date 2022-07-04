47917b2df5fdec69a650102e82a01f34ab8e78cf
Emivw <emiya.vanwyk@gmail.com>
2022-07-04 16:06:22 +0200

    first

---
+++ assets/scripts/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    template: `
+    <div v-if="isVisible" id="box1">
+    <p>{{greeting}}</p>
+    </div>
+    <input type="text" v-model= "greeting"/>
+    <button v-on:click="display">Click Me</button>
+    `,
+    data: function() {
+        return {
+            greeting: "Bonjour",
+            isVisible: true,
+        };
+    },
+    methods: {
+        display() {
+            this.isVisible = !this.isVisible;
+        }
+    }
+});
+app.mount("#app");
